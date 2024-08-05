8d47ba841f576da8c019010b3d6506ce14a741d6
Marcos Arteta <marcos@claimpay.net>
2024-08-05 11:51:46 -0500

    dsadasdas

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '172.16.2.121'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'glpinventorydb'),
             'username' => env('DB_USERNAME', 'dev_glpi_w'),
